207bf49e61636a7e51ee1893d71f4a00b2f58466
Victor L <le.victor@hotmail.com>
2022-03-22 11:06:40 -0500

    added navagation buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello! This works!');
+import './style.css';
+
+
+const mainContent = document.querySelector('#main-content');
+mainContent.setAttribute('style', 'width: 100vw; height: 100vh; background-color: #888');
+
+const navBar = document.createElement('div');
+navBar.classList.add('Navigation-Bar');
+navBar.setAttribute('style', ' display: flex; justify-content: center; align-items: center; width: 100vw; height: 50px; border: 1px solid black');
+mainContent.appendChild(navBar);
+
+const homeBtn = document.createElement('button');
+homeBtn.innerText = 'Home';
+homeBtn.setAttribute('style', "width: 100px; height 50px; margin: 0 5px");
+navBar.appendChild(homeBtn);
+
+const menuBtn = document.createElement('button');
+menuBtn.innerText = 'Menu';
+menuBtn.setAttribute('style', "width: 100px; height 50px; margin: 0 5px");
+navBar.appendChild(menuBtn);
+
+const contactBtn = document.createElement('button');
+contactBtn.innerText = 'Contact';
+contactBtn.setAttribute('style', "width: 100px; height 50px; margin: 0 5px");
+navBar.appendChild(contactBtn);
