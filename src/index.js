9d73aa228f010a27bd5d24b43ed9895316b5c638
V-Le-JLaptop <le.victor@hotmail.com>
2022-03-29 15:49:01 -0500

    repositioned image

--- src/index.js
+++ src/index.js
@@ -59,10 +59,12 @@ mainContainer.appendChild(contentContainer);
 
 const sushiPromoContainer = document.createElement('div');
 sushiPromoContainer.className = 'SushiPromo-Container';
+//sushiPromoContainer.setAttribute('style', 'background-color: #000; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); width: 100%; height: 100%; z-index: -99; overflow: hidden');
 sushiPromoContainer.setAttribute('style', 'background-color: #000; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); width: 100%; height: 100%; z-index: -99; overflow: hidden');
 contentContainer.appendChild(sushiPromoContainer);
 
 const sushiPromo = document.createElement('img');
 sushiPromo.src = sushiPromoImg;
-sushiPromo.setAttribute('style', 'width: auto; height: auto; min-height: 100vh;');
+//sushiPromo.setAttribute('style', 'width: auto; height: auto; min-height: 100vh;');
+sushiPromo.setAttribute('style', 'background-size: cover');
 sushiPromoContainer.appendChild(sushiPromo);
