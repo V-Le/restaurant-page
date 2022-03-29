dce1f6d41e10b25cb930efa6a7957c391af49462
Victor L <le.victor@hotmail.com>
2022-03-29 18:43:07 -0500

    centered image

--- src/index.js
+++ src/index.js
@@ -60,11 +60,10 @@ mainContainer.appendChild(contentContainer);
 const sushiPromoContainer = document.createElement('div');
 sushiPromoContainer.className = 'SushiPromo-Container';
 //sushiPromoContainer.setAttribute('style', 'background-color: #000; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); width: 100%; height: 100%; z-index: -99; overflow: hidden');
-sushiPromoContainer.setAttribute('style', 'background-color: #000; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%); width: 100%; height: 100%; z-index: -99; overflow: hidden');
+sushiPromoContainer.setAttribute('style', 'background-color: #000; width: 100%; height: 100%; position: absolute; top: 0; left: 0; z-index: -99; overflow: hidden');
 contentContainer.appendChild(sushiPromoContainer);
 
 const sushiPromo = document.createElement('img');
 sushiPromo.src = sushiPromoImg;
-//sushiPromo.setAttribute('style', 'width: auto; height: auto; min-height: 100vh;');
-sushiPromo.setAttribute('style', 'background-size: cover');
+sushiPromo.setAttribute('style', 'width: auto; height: auto; min-height: 100vh; position: absolute; top: 50%; left: 50%; transform: translateX(-50%) translateY(-50%);');
 sushiPromoContainer.appendChild(sushiPromo);
