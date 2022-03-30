5a83e14815bbc8088f569852676099d499feb0e4
Victor L <le.victor@hotmail.com>
2022-03-29 23:10:14 -0500

    Moved css code from JS to CSS file

--- src/index.js
+++ src/index.js
@@ -4,55 +4,51 @@ import loadMenu from './pages/menu.js';
 import loadContact from './pages/contact.js';
 import navLogoImg from './assets/logo_1.png';
 
-
-
 const mainContainer = document.querySelector('#main-container');
-mainContainer.setAttribute('style', 'width: 100vw; height: 100vh; display: flex; flex-direction: column; overflow: hidden;');
+
 
 /*******************************/
 /* Creating the navagation Bar */
 /*******************************/
 const navContainer = document.createElement('div');
 navContainer.className = 'Navigation-Container';
-navContainer.setAttribute('style', "display: flex; flex-direction: column; justify-content: center; align-items: center; width: 100vw; height: 151px; background-color: #fff")
 mainContainer.appendChild(navContainer)
 
 const navLogoContainer = document.createElement('div');
 navLogoContainer.className = 'Navigation-Logo-Container';
-navLogoContainer.setAttribute('style', 'display: flex; justify-content: center; align-items: center; width: 100%; height: 100px; backgroundcolor: #fff');
 navContainer.appendChild(navLogoContainer);
 
 const navLogo = document.createElement('img');
+navLogo.id = 'Navigation-Logo-Image';
 navLogo.src = navLogoImg;
-navLogo.setAttribute('style', 'width: 180px; height: auto;')
 navLogoContainer.appendChild(navLogo);
 
-const navBarContainer = document.createElement('div');
-navBarContainer.className = 'Navigation-Bar-Container';
-navBarContainer.setAttribute('style', ' display: flex; justify-content: center; align-items: center; width: 100%; height: 50px; background-color: #888;');
-navContainer.appendChild(navBarContainer);
+const navLinkContainer = document.createElement('div');
+navLinkContainer.className = 'Navigation-Link-Container';
+navContainer.appendChild(navLinkContainer);
+
 
 /*******************************/
 /*     Navagation Buttons      */
 /*******************************/
-
 const homeBtn = document.createElement('button');
 homeBtn.innerText = 'Home';
 homeBtn.addEventListener('click', loadHome);
-navBarContainer.appendChild(homeBtn);
+navLinkContainer.appendChild(homeBtn);
 
 const menuBtn = document.createElement('button');
 menuBtn.innerText = 'Menu';
 menuBtn.addEventListener('click', loadMenu)
-navBarContainer.appendChild(menuBtn);
+navLinkContainer.appendChild(menuBtn);
 
 const contactBtn = document.createElement('button');
 contactBtn.innerText = 'Contact';
 contactBtn.addEventListener('click', loadContact)
-navBarContainer.appendChild(contactBtn);
+navLinkContainer.appendChild(contactBtn);
 
 const allBtns = document.querySelectorAll('button');
-allBtns.forEach(btns => btns.setAttribute('style', "width: 100px; height 50px; margin: 0 5px"))
+allBtns.forEach(btns => btns.className = 'Navigation-Buttons');
+
 
 /*******************************/
 /* Creating the lower content  */
